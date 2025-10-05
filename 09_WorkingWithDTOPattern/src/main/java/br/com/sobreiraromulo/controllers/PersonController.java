330e503f3c2d6783f674996b1e6681cdd74e2557
Romulo Sobreira <sobreiaromuloes@gmail.com>
2025-10-05 06:10:14 -0300

    feat: add new modules

---
+++ 09_WorkingWithDTOPattern/src/main/java/br/com/sobreiraromulo/controllers/PersonController.java
@@ -0,0 +1,46 @@
+package br.com.sobreiraromulo.controllers;
+
+import br.com.sobreiraromulo.data.dto.PersonDTO;
+import br.com.sobreiraromulo.services.PersonServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("person")
+public class PersonController {
+
+    @Autowired
+    private PersonServices personServices;
+
+    @GetMapping("/{id}")
+    public PersonDTO findById(@PathVariable("id") Long id) {
+        return personServices.findById(id);
+    }
+
+    @GetMapping
+    public List<PersonDTO> findAll() {
+        return personServices.findAll();
+    }
+
+
+    @PostMapping
+    public PersonDTO create(@RequestBody PersonDTO person) {
+        return personServices.create(person);
+    }
+
+    @PutMapping
+    public PersonDTO update(@RequestBody PersonDTO person) {
+        return personServices.update(person);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
+        personServices.delete(id);
+
+        return  ResponseEntity.noContent().build();
+    }
+}
