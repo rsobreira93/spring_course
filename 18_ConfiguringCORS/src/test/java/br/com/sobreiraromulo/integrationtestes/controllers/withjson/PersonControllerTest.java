9e62550d4b4e2aaf43b8e48d25170fa1fb7ce73a
Romulo Sobreira <sobreiaromuloes@gmail.com>
2025-10-14 06:56:43 -0300

    feat: add module 18

---
+++ 18_ConfiguringCORS/src/test/java/br/com/sobreiraromulo/integrationtestes/controllers/withjson/PersonControllerTest.java
@@ -0,0 +1,33 @@
+package br.com.sobreiraromulo.integrationtestes.controllers.withjson;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
